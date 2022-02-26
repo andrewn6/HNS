8c31acb49ade056ed747742d94866b7d52875f80
Andrew <andrewnijmeh1@gmail.com>
2022-02-26 14:55:31 -0500

    the rest

---
+++ contracts/Domains.sol
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.10;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import {Base64} from "./libraries/Base64.sol";
+import {StringUtils} from "./libraries/StringUtils.sol";
+import "hardhat/console.sol";
+
+contract Domains is ERC721URIStorage {
+
+    // track token ID
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    string public tld;
+       
+    mapping(string => address) public domains;
+    mapping(string => string) public records;
+
+  constructor(string memory _tld) payable ERC721("Hack Name Service", "HNS") {
+    tld = _tld;
+    console.log("Name service deployed:", _tld);
+  }
+
+  function register(string calldata name) public {
+    require(domains[name] == address(0));
+
+    uint256 _price = price(name);
+    require(msg.value >= _price, "Not enough Matic paid");
+
+    string memory _name = string(abi.encodePacked(name, ".", tld));
+    string memory finalSvg = string(abi.encodePacked(svgPartOne, _name, svgPartTwo));
+    // check current token id on current record
+    uint256 newRecordId _tokenIds.current();
+  }
+  
+  function price(string calldata) public pure returns(uint) {
+    uint len StringUtils.strlen(name);
+    require(len > 0);
+    if (len == 3) {
+      return 5 * 10**17;
+    } else if (len == 4) {
+      return 3 * 10**17;
+    } else {
+      return 1 * 10**17;
+    }
+  }
+  
+  function register(string calldata name) public payable {
+    require(domains[name] == address(0));
+
+    uint _price = price(name);
+
+    require(msg, value >= price, "Not enough MATIC paid");
+
+    domains[name] = msg.sender;
+    console.log("%s has registered a domain!", msg.sender);
+  }
+
+  function getAddress(string calldata name) public view returns (address) {
+    return domains[name];
+  }
+
+  function setRecord(string calldata name, string calldata record) public {
+    require(domains[name] == msg.sender);
+    records[name] = record;
+  }
+
+  function getRecord(string calldata name) publix view returns(string memory) {
+    return records[name];
+  }
+}
---
+++ scripts/run.js
@@ -0,0 +1,30 @@
+const main = async () => {
+  const [owner, randomPerson] = await hre.ethers.getSigners();
+  const domainContractFactory = await hre.ethers.getContractFactory('Domains');
+
+  const domainContract = await domainContractFactory.deploy("hack");
+  await domainContract.deployed();
+
+  console.log("Contract deployed to:", domainContract.address);
+ 
+  let txn = await domainContract.register("hack", {value: hre.ethers.utils.parseEther('0.1')});
+  await txn.wait();
+  
+  const address = await domainContract.getAddress("hack");
+  console.log("Owner of domain hack:", address);
+
+  const balance = await hre.ethers.provider.getBalance(domainContract.address);
+  console.log("Contract balance:" hre.ethers.utils.formatEther(balance));
+};
+
+const runMain = async() => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
